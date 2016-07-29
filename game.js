b8696fd7555d87811156ae734a8ad9d19aeeae12
eliserothberg <rothberg.elise@gmail.com>
2016-07-29 11:47:00 -0700

    fixed player selection

--- game.js
+++ game.js
@@ -35,61 +35,25 @@ stats = '';
 
   console.log('These are ike\'s heath Points: ' + ike.healthPoints);
 
-function pickPlayer() {
-    $('.playerArea').on('click', function() {
-      player = $( ).data("character");
-      $(this).removeClass('playerArea');
-      // $('<span id="character-health"></span>').appendTo('#player')
+  
 
+
+function pickPlayer() {
+    $(document).on("click", ".playerArea", function () {
+      player = $(this).data("character");
+      // event.preventDefault();
+      // player = $( ).data("character");
+      console.log('Player: ' + player);
+      $(this).removeClass('newArea');
+    
       $('.playerArea').not(this).each(function(){
         $(this).removeClass('playerArea');
-
+        $(this).addClass('newArea');
         $(this).css( 'border-color', 'red'); 
-        $(this).toggleClass('playerArea', 'enemyArea');
         $(this).clone().appendTo($('.enemyArea'));
         $(this).remove(); 
-        console.log(player);
-
-        // var elements = $(this);
-        //   elements.css( 'border-color', 'black');
-        //   elements.clone().appendTo($('#defenderArea'));
-        //   console.log(defender + ' chosen');
-
-  // if($('#ike').on('click')) {
-  //           $('#ike').css( 'border-color', 'black')
-  //           $('#ike').clone().appendTo($('#defenderArea'))
-  //           $('#ike').remove()
-  //           $('#ike').appendTo('#defender')
-  //           ike = defender;
-  //            console.log($('#defender').val());
-  //             console.log($('#ike').val());
-  //           console.log('ike chosen');
-  //         }
-          
-
-  //         if($('#maggie').on('click')) {
-  //           $('#maggie').css( 'border-color', 'black')
-  //           $('#maggie').clone().appendTo($('#defenderArea'))
-  //           $('#maggie')($('#defenderArea'))
-  //           $('#maggie').appendTo('#defender')
-  //           // maggie = defender;
-  //           console.log('maggie chosen');
-  //         }
-  //         else if($('#michelle').on('click')) {
-  //           $('#michelle').css( 'border-color', 'black')
-  //           $('#michelle').clone().appendTo($('#defenderArea'))
-  //           $('#michelle').remove()
-  //           $('#michelle').appendTo('#defender')
-  //           // michelle = defender;
-  //           console.log('michelle chosen');
-  //         }
-  //         else {
-  //           $('#stewie').css( 'border-color', 'black');
-  //           $('#stewie').clone().appendTo($('#defenderArea'))
-  //           $('#stewie').appendTo('#defender')
-  //           $('#stewie').remove()
-  //           // stewie = defender;
-  //           console.log('stewie chosen');
+       
+      
       });
     });
 }
@@ -98,71 +62,32 @@ function pickPlayer() {
  
     function pickDefender () {     
   
-        $('.enemyArea').on('click', function () {
-          defender = $( ).data("character");
+        $(document).on("click", ".newArea", function () {
+          defender = $(this).data("character");
+         // event.preventDefault();
+         console.log('Defender: ' + defender);
+         
+         $(this).removeClass('newArea');
+         $('<span id="character-health"></span>').appendTo('#defender')
 
-        // $(this).removeClass('enemyArea');
-         // $('<span id="character-health"></span>').appendTo('#defender')
-
-        // $(this).css( 'border-color', 'black'); 
-        // $(this).toggleClass('enemyArea', 'defenderArea');
-        // $(this).clone().appendTo($('#defenderArea'));
-        // $(this).remove(); 
-        // console.log(defender);
-        
-        // var elements = $(this);
-        //   elements.css( 'border-color', 'black');
-        //   elements.clone().appendTo($('#defenderArea'));
-        //   console.log(defender + ' chosen');
-
-  if($('#ike').on('click')) {
-            $('#ike').css( 'border-color', 'black')
-            $('#ike').clone().appendTo($('#defenderArea'))
-            $('#ike').remove()
-            $('#ike').appendTo('#defender')
-            ike = defender;
-             console.log($('#defender').val());
-              console.log($('#ike').val());
-            console.log('ike chosen');
-          }
-          
-
-          if($('#maggie').on('click')) {
-            $('#maggie').css( 'border-color', 'black')
-            $('#maggie').clone().appendTo($('#defenderArea'))
-            $('#maggie')($('#defenderArea'))
-            $('#maggie').appendTo('#defender')
-            // maggie = defender;
-            console.log('maggie chosen');
-          }
-          else if($('#michelle').on('click')) {
-            $('#michelle').css( 'border-color', 'black')
-            $('#michelle').clone().appendTo($('#defenderArea'))
-            $('#michelle').remove()
-            $('#michelle').appendTo('#defender')
-            // michelle = defender;
-            console.log('michelle chosen');
-          }
-          else {
-            $('#stewie').css( 'border-color', 'black');
-            $('#stewie').clone().appendTo($('#defenderArea'))
-            $('#stewie').appendTo('#defender')
-            $('#stewie').remove()
-            // stewie = defender;
-            console.log('stewie chosen');
-          }
-  
+         $(this).css( 'border-color', 'black'); 
+         $(this).clone().appendTo($('#defenderArea'));
+         $(this).remove(); 
+      
         });
     }
 
     pickDefender();
 
-        $('#attack').on('click', function() {
-          console.log('Ike\'s hp ' + ike.healthPoints + ' maggie\'s hp ' + maggie.healthPoints);
 
-          var player = maggie;
-          var defender = ike;
-                    console.log('Defender\'s hp ' + defender.healthPoints + ' Player\'s hp ' + player.healthPoints);
+        $('#attack').on('click', function() {
+          console.log('Defender: ' + defender);
+          console.log('Player: ' + player);
+          // console.log('Ike\'s hp ' + ike.healthPoints + ' maggie\'s hp ' + maggie.healthPoints);
+
+          // var player = maggie;
+          // var defender = ike;
+                    // console.log('Defender\'s hp ' + defender.healthPoints + ' Player\'s hp ' + player.healthPoints);
 
 
           if(( defender.healthPoints > 0) && (player.healthPoints > 0)){      
@@ -194,44 +119,44 @@ function pickPlayer() {
 
       });
 
-        function pickNewDefender () {
+        // function pickNewDefender () {
 
-            $('.enemyArea').on('click', function () {
-              $('#statusUpdate').html('Next!');
-              $('#win-loss').html('');
+        //     $('.enemyArea').on('click', function () {
+        //       $('#statusUpdate').html('Next!');
+        //       $('#win-loss').html('');
 
-              if($('#stewie').on('click')){
-                $('#stewie').css( 'border-color', 'black')
-                $('#stewie').clone().appendTo($('#defenderArea'))
-                $('#stewie').appendTo('#defender')
-                $('#stewie').remove()
-                console.log('stewie chosen');
-                defender = stewie;
-              }
-              else if($('#maggie').on('click')) {
-              $('#maggie').css( 'border-color', 'black')
-              $('#maggie').clone().appendTo($('#defenderArea'))
-              $('#maggie').remove()
-              $('#maggie').appendTo('#defender')
-              console.log('maggie chosen');
-              }
-              else if($('#michelle').on('click')) {
-                $('#michelle').css( 'border-color', 'black')
-                $('#michelle').clone().appendTo($('#defenderArea'))
-                $('#michelle').remove()
-                $('#michelle').appendTo('#defender')
-                console.log('michelle chosen');
-              }
-              else {
-                $('#ike').css( 'border-color', 'black')
-                $('#ike').clone().appendTo($('#defenderArea'))
-                $('#ike').remove()
-                $('#ike').appendTo('#defender')
-                console.log('ike chosen');
-              }
-            });   
-          };
-          pickNewDefender ();
+        //       if($('#stewie').on('click')){
+        //         $('#stewie').css( 'border-color', 'black')
+        //         $('#stewie').clone().appendTo($('#defenderArea'))
+        //         $('#stewie').appendTo('#defender')
+        //         $('#stewie').remove()
+        //         console.log('stewie chosen');
+        //         defender = stewie;
+        //       }
+        //       else if($('#maggie').on('click')) {
+        //       $('#maggie').css( 'border-color', 'black')
+        //       $('#maggie').clone().appendTo($('#defenderArea'))
+        //       $('#maggie').remove()
+        //       $('#maggie').appendTo('#defender')
+        //       console.log('maggie chosen');
+        //       }
+        //       else if($('#michelle').on('click')) {
+        //         $('#michelle').css( 'border-color', 'black')
+        //         $('#michelle').clone().appendTo($('#defenderArea'))
+        //         $('#michelle').remove()
+        //         $('#michelle').appendTo('#defender')
+        //         console.log('michelle chosen');
+        //       }
+        //       else {
+        //         $('#ike').css( 'border-color', 'black')
+        //         $('#ike').clone().appendTo($('#defenderArea'))
+        //         $('#ike').remove()
+        //         $('#ike').appendTo('#defender')
+        //         console.log('ike chosen');
+        //       }
+        //     });   
+        //   };
+        //   pickNewDefender ();
           };
 
         });
